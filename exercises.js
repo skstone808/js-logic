86f7fe99e29388cc8182dcdc2f1cf744fc2cc286
Keola Stone <skstone808@gmail.com>
2017-03-10 21:43:03 -1000

    Pau #3

--- exercises.js
+++ exercises.js
@@ -68,6 +68,12 @@ console.log(login("test1234"));
  * Console.log your result.
 */
 
+function isGreaterThan(first, second){
+  if(first > second){
+    return "true";
+  }
+}
+console.log(isGreaterThan(3,2));
 
 /*
  * #4
