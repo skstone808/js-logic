9b1e5a897c85a6f0e80ffb7ef1cbf5f6dac98b2a
Keola Stone <skstone808@gmail.com>
2017-03-10 23:28:44 -1000

    #7 pau

--- exercises.js
+++ exercises.js
@@ -147,6 +147,15 @@ console.log(isEqual(1,1));
  * Console.log your result.
 */
 
+function notEqual(first, second){
+  if(first !== second){
+    return "Opposites do attract.";
+  }else{
+    return "Cause it's like you're my mirror.";
+  }
+}
+console.log(notEqual(1,2));
+
 
 /*
  * #8
